d6c2210da6d887da6be361505d23f98ee14c80c3
Wulian233 <1055917385@qq.com>
2025-08-09 19:52:41 +0800

    Backport to 1.20-1.20.4

--- neoforge/src/main/java/com/wulian/texturelocaleredirector/neoforge/TextureLocaleRedirectorClientNeoForge.java
+++ neoforge/src/main/java/com/wulian/texturelocaleredirector/neoforge/TextureLocaleRedirectorClientNeoForge.java
@@ -1,11 +1,15 @@
 package com.wulian.texturelocaleredirector.neoforge;
 
 import com.wulian.texturelocaleredirector.TextureLocaleRedirector;
-import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.IExtensionPoint.DisplayTest;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 
-@Mod(value = TextureLocaleRedirector.MOD_ID, dist = Dist.CLIENT)
+@Mod(value = TextureLocaleRedirector.MOD_ID)
 public class TextureLocaleRedirectorClientNeoForge {
     public TextureLocaleRedirectorClientNeoForge() {
+        ModContainer container = ModList.get().getModContainerById(TextureLocaleRedirector.MOD_ID).orElseThrow();
+        container.registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 }
