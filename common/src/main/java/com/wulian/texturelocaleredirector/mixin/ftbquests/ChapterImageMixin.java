ac6ba9a4128277111a17580f7cbb28650eb52362
Wulian233 <1055917385@qq.com>
2025-08-23 15:29:47 +0800

    支持替换FTB任务图片（Mixin插件 可选）

---
+++ common/src/main/java/com/wulian/texturelocaleredirector/mixin/ftbquests/ChapterImageMixin.java
@@ -0,0 +1,30 @@
+package com.wulian.texturelocaleredirector.mixin.ftbquests;
+
+import com.wulian.texturelocaleredirector.LangTextureCache;
+import com.wulian.texturelocaleredirector.TextureLocaleRedirector;
+import dev.ftb.mods.ftblibrary.icon.Icon;
+import dev.ftb.mods.ftbquests.quest.ChapterImage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = ChapterImage.class, remap = false)
+public abstract class ChapterImageMixin {
+    @Inject(method = "setImage(Ldev/ftb/mods/ftblibrary/icon/Icon;)Ldev/ftb/mods/ftbquests/quest/ChapterImage;", at = @At("HEAD"), cancellable = true)
+    private void injectLocalizedImage(Icon image, CallbackInfoReturnable<ChapterImage> cir) {
+        String currentLang = LangTextureCache.getCurrentLanguage();
+        if ("en_us".equals(currentLang)) {
+            return;
+        }
+
+        String originalPath = image.toString();
+        String localizedPath = "textures/" + currentLang + "/" + originalPath.replaceFirst("^textures/", "");
+        Icon localizedIcon = Icon.getIcon(localizedPath);
+
+        if (!localizedIcon.isEmpty()) {
+            TextureLocaleRedirector.LOGGER.info("Redirected ChapterImage icon {} -> {}", originalPath, localizedPath);
+            cir.setReturnValue(((ChapterImage) (Object) this).setImage(localizedIcon));
+        }
+    }
+}
---
+++ common/src/main/java/com/wulian/texturelocaleredirector/mixin/ftbquests/FTBQMixinPlugin.java
@@ -0,0 +1,49 @@
+package com.wulian.texturelocaleredirector.mixin.ftbquests;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class FTBQMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.endsWith("ChapterImageMixin")) {
+            try {
+                Class.forName("dev.ftb.mods.ftbquests.quest.ChapterImage");
+                return true;
+            } catch (ClassNotFoundException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return List.of();
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
